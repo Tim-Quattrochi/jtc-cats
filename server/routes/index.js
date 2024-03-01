acd610b26b0a98ebb2ad7244f3be3300d1d10e50
Tim Quattrochi <timq82@gmail.com>
2024-02-29 22:04:23 -0500

    Refactor server.js to use router for API endpoints

---
+++ server/routes/index.js
@@ -0,0 +1,69 @@
+const { Router } = require("express");
+const fs = require("fs");
+
+const router = Router();
+
+/**
+ * GET /api/create/:breed
+ * @param req.params.breed - the breed of the cat
+ */
+router.get("/create/:breed", (req, res) => {
+  const { breed } = req.params;
+  const options = {
+    headers: {
+      "x-api-key": process.env.CAT_API_KEY,
+    },
+  };
+
+  /**
+   * Check if the breed is already saved in the "cats" dir
+   * If yes, send the data from the file
+   * If not, call the getCat function
+   */
+  const checkForBreed = (res) => {
+    fs.readdir("./cats", (err, files) => {
+      if (err) {
+        console.error(err);
+        return res.status(500).json({ message: "Server error" });
+      } else {
+        let found = false;
+        files.forEach((file) => {
+          const data = fs.readFileSync(`./cats/${file}`);
+          const persistedCat = JSON.parse(data);
+
+          if (persistedCat[0]["name"].toLowerCase().includes(breed)) {
+            found = true;
+            return res.status(200).send(persistedCat);
+          }
+        });
+
+        if (!found) {
+          getCat();
+        }
+      }
+    });
+  };
+
+  const getCat = async () => {
+    try {
+      const response = await fetch(
+        `https://api.thecatapi.com/v1/breeds/search?q=${breed}`,
+        options
+      );
+
+      const data = await response.json();
+      let id = data[0]["image"]["id"];
+
+      //save data to file to the "cats" dir
+      fs.writeFileSync(`./cats/${id}.json`, JSON.stringify(data));
+      return res.status(201).send(data);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: "Server error" });
+    }
+  };
+
+  checkForBreed(res);
+});
+
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -1,8 +1,7 @@
 const dotenv = require("dotenv").config();
 const cors = require("cors");
 const express = require("express");
-const fs = require("fs");
-const https = require("https");
+const router = require("./routes/index");
 
 const app = express();
 
@@ -13,39 +12,6 @@ app.use((req, res, next) => {
   next();
 });
 
-app.get("/create/:q", (req, res) => {
-  const query = req.params.q;
-  const options = {
-    hostname: "api.thecatapi.com",
-    path: `/v1/breeds/search?q=${query}`,
-    method: "GET",
-    headers: {
-      "x-api-key": process.env.CAT_API_KEY,
-    },
-  };
-
-  const apiRequest = https.request(options, (apiResponse) => {
-    let data = "";
-
-    apiResponse.on("data", (chunk) => {
-      data += chunk;
-    });
-
-    apiResponse.on("end", () => {
-      let id = JSON.parse(data)[0]["image"]["id"];
-      console.log(data);
-      // `${id}.json`
-      //save data to file to the "cats" dir
-      fs.writeFileSync(`./cats/${id}.json`, data);
-      res.status(201).send(data);
-    });
-  });
-
-  apiRequest.on("error", (error) => {
-    console.error(error);
-  });
-
-  apiRequest.end();
-});
+app.use("/api", router);
 
 app.listen(3001, () => console.log("Server is running"));
