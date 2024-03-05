a2fd9e59f0ec47a7d59e1e6731b008d293c84865
Tim Quattrochi <timq82@gmail.com>
2024-03-05 17:17:47 -0500

    Refactor helpers.js: Add new functions and update existing ones

--- client/src/utils/helpers.js
+++ client/src/utils/helpers.js
@@ -1,7 +1,5 @@
 export function refreshHTML(element) {
-  if (element instanceof HTMLElement) {
-    element.innerHTML = "";
-  }
+  element.innerHTML = "";
 }
 
 export const randomBreed = [
@@ -33,4 +31,44 @@ export function showAToast(message, level) {
   setTimeout(() => {
     toast.remove();
   }, 3000);
+}
+
+export function whichCatsHaveBeenFetched(
+  list,
+  cat,
+  forDelete = false
+) {
+  let updatedList;
+
+  // if this function is being used to delete, only the id is passed.
+  // if it is being used to add, it's an object.
+  const isCatInList = list.some((catObj) =>
+    cat.id ? catObj.id === cat.id : catObj.id === cat
+  );
+
+  if (!forDelete && !isCatInList) {
+    updatedList = [...list, cat];
+  } else {
+    updatedList = list.filter((catObj) => catObj.id !== cat);
+  }
+
+  return updatedList;
+}
+
+export function makeItRandom() {
+  return Math.floor(Math.random() * randomBreed.length);
+}
+
+/**
+ * @param {string} tag HTML element name.
+ * @param {string} classes className to add.
+ * @param {string} [text=""] text to add if any.
+ * @returns {HTMLElement} HTML element.
+ * @example createElement("div", "previewContainer");
+ */
+export function createStyledElement(tag, classes, text = "") {
+  const element = document.createElement(tag);
+  element.classList.add(classes);
+  element.textContent = text;
+  return element;
 }
