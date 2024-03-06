b0d141a897cd78536ed1d1df8f56cf7558e89242
Tim Quattrochi <timq82@gmail.com>
2024-03-06 08:43:24 -0500

    refactor, better naming of funcs/vars

--- client/src/code.js
+++ client/src/code.js
@@ -5,17 +5,16 @@ import {
   makeItRandom,
   whichCatsHaveBeenFetched,
   createStyledElement,
+  loopAndRender,
+  makeEditable,
 } from "./utils/helpers.js";
 
 //elements
-const saveBtn = document.querySelector("#save");
 const catPic = document.querySelector(".catPic");
 const form = document.querySelector("form");
 const catCard = document.querySelector(".catCard");
 const title = document.querySelector(".breedName");
-
 const descr = document.createElement("p");
-const previewContainer = document.createElement("aside");
 
 const addToCollection = createStyledElement(
   "button",
@@ -23,57 +22,25 @@ const addToCollection = createStyledElement(
   "Add to collection"
 );
 
-let input,
-  catId,
-  changed = false,
-  catIdToDelete = [],
+let catId,
+  catIdToDelete,
   cats = 0;
 
 let catsIdFetched = [];
 
-function loopAndRender(list) {
-  refreshHTML(previewContainer);
-  for (let i = 0; i < list.length; i++) {
-    let container = createStyledElement("div", "previewContainer");
-
-    previewContainer.classList.add("previewContainer");
-
-    let img = createStyledElement("img", "previewCat");
-    let deleteIcon = createStyledElement(
-      "span",
-      "previewDelete",
-      "X"
-    );
-
-    img.src = list[i].url;
-    img.alt = list[i].name;
-
-    document.body.append(container);
-    previewContainer.append(container);
-    document.body.append(previewContainer);
-
-    container.append(deleteIcon);
-    container.append(img);
-
-    deleteIcon.addEventListener("click", async (e) => {
-      e.stopPropagation();
-      await deleteCat(e);
-    });
-  }
-}
-
+/**
+ * Deletes a cat.
+ * @param {Event} e - The event object.
+ * @returns {Promise<void>} - A promise that resolves when the cat is deleted.
+ */
 async function deleteCat(e) {
   e.preventDefault();
   e.stopPropagation();
 
+  // variables to determine which dynamic img was clicked get then get the id from
+  // the filename.
   const elementClicked = e.target;
   const parentElement = elementClicked.parentElement;
-  const previousSibling = parentElement.previousElementSibling;
-  const nextSibling = parentElement.nextElementSibling;
-
-  if (previousSibling === null && nextSibling === null) {
-    previewContainer.remove();
-  }
 
   catIdToDelete = parentElement
     .querySelector("img")
@@ -105,7 +72,8 @@ async function deleteCat(e) {
 
       showAToast("Cat deleted", "success");
       loopAndRender(
-        whichCatsHaveBeenFetched(catsIdFetched, id, true)
+        whichCatsHaveBeenFetched(catsIdFetched, id, true),
+        deleteCat
       );
     } else {
       showAToast("Cat not deleted", "error");
@@ -124,7 +92,6 @@ async function getCat(e) {
   e.stopPropagation();
 
   cats++; //increment the cats fetched.
-  console.log(cats);
 
   const response = await fetch(
     `http://localhost:3001/api/cats/${randomBreed[makeItRandom()]}`
@@ -133,15 +100,15 @@ async function getCat(e) {
   //refresh the elements
   refreshHTML(title);
   refreshHTML(descr);
-  let editableH1 = document.querySelector("#editable1");
 
-  if (editableH1) {
-    editableH1.remove();
+  // check and remove the editable name element if it exists from previous.
+  let editableName = document.querySelector("#editableName");
+
+  if (editableName) {
+    editableName.remove();
   }
 
   const data = await response.json();
-
-  console.log("RES: ", data);
 
   const { image: cat, name, description, nickName } = data[0];
 
@@ -157,86 +124,24 @@ async function getCat(e) {
   catCard.append(addToCollection);
 
   //push the newly fetched cat to the array.
-
   catsIdFetched.push(cat);
+
+  makeEditable("editableName", "save", catId);
 
   addToCollection.addEventListener("click", async (e) => {
     showAToast("Cat added to collection.", "success");
-    loopAndRender(whichCatsHaveBeenFetched(catsIdFetched, cat));
+    loopAndRender(
+      whichCatsHaveBeenFetched(catsIdFetched, cat),
+      deleteCat
+    );
   });
 }
-
-saveBtn.addEventListener("click", function () {
-  const h1 = document.createElement("h1");
-  h1.id = "editable";
-  h1.innerText = input.value;
-  input.replaceWith(h1);
-  this.style.display = "none";
-
-  title.removeEventListener("click", titleClickHandler);
-});
 
 form.addEventListener("submit", async (e) => {
   await getCat(e);
 });
 
-function makeEditable(h1Id, buttonId) {
-  let h1 = document.querySelector("h2");
-  let button = document.querySelector(`#${buttonId}`);
-
-  let input;
-
-  const h1ClickHandler = function () {
-    input = document.createElement("input");
-    input.value = this.innerText;
-    this.replaceWith(input);
-    button.style.display = "inline";
-  };
-
-  h1.addEventListener("click", h1ClickHandler);
-
-  button.addEventListener("click", function () {
-    h1 = document.createElement("h1");
-    h1.id = h1Id;
-    h1.innerText = input.value;
-    input.replaceWith(h1);
-    this.style.display = "none";
-
-    h1.addEventListener("click", h1ClickHandler);
-
-    if (catId) {
-      fetch(`http://localhost:3001/api/cats/${catId}`, {
-        method: "PUT",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ name: input.value }),
-      })
-        .then((response) => response.json())
-        .then((data) => {
-          console.log(data);
-          changed = true;
-        })
-        .catch((error) => {
-          console.error("Error:", error);
-        });
-    }
-  });
-}
-
-makeEditable("editable1", "save");
-
 //first load.
-
 window.addEventListener("load", () =>
   showAToast("Click 'Get a cat' to get your first cat", "success")
 );
-
-//The user first visits the site.
-//They are greeted with a "Get a cat" button.
-//The user clicks the button, and a random breed is fetched.
-//The cat's name and description are displayed on the page.
-//This is the READ of CRUD.
-
-//The user can give the cat a nickname.
-//This is the UPDATE of CRUD.
--- client/src/utils/helpers.js
+++ client/src/utils/helpers.js
@@ -22,6 +22,7 @@ export const randomBreed = [
  * @param {string} message  message to display.
  * @param {string} level success | error
  */
+
 export function showAToast(message, level) {
   const toast = document.createElement("div");
   toast.className = "toast";
@@ -66,9 +67,102 @@ export function makeItRandom() {
  * @returns {HTMLElement} HTML element.
  * @example createElement("div", "previewContainer");
  */
+
 export function createStyledElement(tag, classes, text = "") {
   const element = document.createElement(tag);
   element.classList.add(classes);
   element.textContent = text;
   return element;
+}
+
+/**
+ * Renders a list of items by creating preview elements and attaching event listeners for deletion.
+ *
+ * @param {Array} list - The list of cats to render.
+ * @param {Function} deleteCb - The callback function to be executed when an item is deleted.
+ */
+
+export function loopAndRender(list, deleteCb) {
+  document.querySelector(".previewContainer")?.remove();
+  const previewContainer = createStyledElement(
+    "aside",
+    "previewContainer"
+  );
+
+  for (let i = 0; i < list.length; i++) {
+    let container = createStyledElement("div", "previewContainer");
+
+    let img = createStyledElement("img", "previewCat");
+    let deleteIcon = createStyledElement(
+      "span",
+      "previewDelete",
+      "X"
+    );
+
+    img.src = list[i].url;
+    img.alt = list[i].name;
+
+    document.body.append(container);
+    previewContainer.append(container);
+    document.body.append(previewContainer);
+
+    container.append(deleteIcon);
+    container.append(img);
+
+    deleteIcon.addEventListener("click", async (e) => {
+      e.stopPropagation();
+      await deleteCb(e);
+    });
+  }
+}
+
+/**
+ * Makes an element editable and updates its value when the save button is clicked.
+ * @param {string} elementId - The ID of the element to make editable.
+ * @param {string} buttonId - The ID of the save button.
+ * @param {string} catId - The ID of the cat to edit
+ */
+
+export function makeEditable(elementId, buttonId, catId) {
+  let nameElement = document.querySelector("h2");
+  let saveBtn = document.querySelector(`#${buttonId}`);
+
+  let input;
+
+  const nameElementClickHandler = function () {
+    input = document.createElement("input");
+    input.value = this.innerText;
+    this.replaceWith(input);
+    saveBtn.style.display = "inline";
+  };
+
+  nameElement.addEventListener("click", nameElementClickHandler);
+
+  saveBtn.addEventListener("click", function () {
+    nameElement = document.createElement("h2");
+    nameElement.id = elementId;
+    nameElement.innerText = input.value;
+    input.replaceWith(nameElement);
+    this.style.display = "none";
+    // make the element clickable/editable again.
+    nameElement.addEventListener("click", nameElementClickHandler);
+
+    if (catId) {
+      fetch(`http://localhost:3001/api/cats/${catId}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ name: input.value }),
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data);
+          showAToast("Cat name changed!", "success");
+        })
+        .catch((error) => {
+          console.error("Error:", error);
+        });
+    }
+  });
 }
